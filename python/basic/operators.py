1cc80eca167add729f0287a596ad226b2c452619
Gabriel Michael Ojomakpene <gabrielmicheal497@gmail.com>
2024-09-29 19:54:56 +0100

    feat: added basic operators tutorial

---
+++ python/basic/operators.py
@@ -0,0 +1,36 @@
+# Python operators
+
+# Addition
+
+a = 10
+b = 5
+c = a + b
+
+print("c", c)
+
+# Subtraction
+
+d = a - b
+
+print("d", d)
+
+# Multiplication
+
+e = a * b
+
+print("e", e)
+
+# Division
+
+# Floor division
+
+f = a / b
+g = a // b
+
+print("f", f)
+print("g", g)
+
+# Modulus
+
+print("5 % 3 =", 5 % 3)
+print("4 % 2 =", 4 % 2)
