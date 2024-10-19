b980741af8333a136a5f0b33daa5b5774073198e
Gabriel Michael Ojomakpene <gabrielmicheal497@gmail.com>
2024-10-19 11:05:39 +0100

    feat: more on functions

--- python/basic/function.py
+++ python/basic/function.py
@@ -35,6 +35,7 @@ def login():
 
 correct_guess = 5
 
+
 def guessing_game():
     user_guess = input("Enter a guess: ")
 
@@ -48,4 +49,68 @@ def guessing_game():
         guessing_game()
 
 
-guessing_game()
+# guessing_game()
+
+"""
+* DRY -> Don't repeat yourself
+"""
+
+# * Function parameters and arguments
+
+
+def add_numbers(first_number, second_number):
+    # * Parameters are passed when defining a function
+    # * Arguments are passed when calling a function
+
+    return first_number + second_number
+
+
+solution = add_numbers(12, 34)
+
+print(solution)
+
+# print(add_numbers(9, 50))
+# print(add_numbers(2, 2))
+# print(add_numbers(5, 0))
+# print(add_numbers(5, -4))
+# print(add_numbers(6, 10))
+
+
+# * Types of arguments in a function
+
+# * 1. Positional argument
+# * 2. Keyword argument
+# * 3. Optional parameters
+
+
+def positional_argument(a, b, c):
+    print("a", a)
+    print("b", b)
+    print("c", c)
+
+
+# positional_argument(1, 2, 3)
+
+
+def keyword_argument(a, b=None, c=None):
+    print("a", a)
+    print("b", b)
+    print("c", c)
+
+
+keyword_argument(1, c="Ruth", b="Orire")
+
+# * Rule => Positional argument cannot come after a keyword argument
+
+"""
+* Assignment
+
+? Write a function to perform the following actions
+
+* Subtract numbers => subtract_numbers() => Use positional arguments only
+* Multiply numbers => multiply_numbers() => Use keyword arguments only
+* Divide numbers using float division => divide_numbers() => Use positional and keyword arguments
+* Get remainder when two numbers are divided => get_remainder() => Use positional arguments only
+
+! Your functions should only use the return keyword and not print directly from it.
+"""
