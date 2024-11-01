f661eba481518d1903d469161194923b7b4305d3
Gabriel Michael Ojomakpene <gabrielmicheal497@gmail.com>
2024-11-01 03:47:45 +0100

    feat: completed calculator project UI

--- CalculatorApp/app.py
+++ CalculatorApp/app.py
@@ -5,6 +5,10 @@ kivy.require("2.3.0")
 from kivy.app import App
 from kivy.core.window import Window
 from kivy.uix.widget import Widget
+from kivy.core.text import LabelBase, DEFAULT_FONT
+
+# Register the Google Font as the default font for the app
+LabelBase.register(DEFAULT_FONT, "./fonts/Poppins-Medium.ttf")
 
 Window.size = (300, 500)  # Set window size to 300x500 pixels
 
@@ -15,6 +19,8 @@ class CalculatorWidget(Widget):
 
 class CalculatorApp(App):
     def build(self):
+        Window.clear_color = 36 / 255, 38 / 255, 42 / 255, 1
+
         return CalculatorWidget()
 
 
